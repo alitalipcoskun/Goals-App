9c7c223d5bfc2f7e80b0ae931d23f72ab05219f1
Ali Talip COŞKUN <ali_talip_18@hotmail.com>
2023-02-13 22:42:18 +0300

    Feedback mechanism

--- goals/src/Components/CourseForm/CourseForm.js
+++ goals/src/Components/CourseForm/CourseForm.js
@@ -1,58 +1,69 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import './course-form.css'
 
 
-function CourseForm(props){
+function CourseForm(props) {
     const [formChecker, formHandler] = useState(false);
-    
+    const [isValid, setIsValid] = useState(true); // to give to user a feedback message via CSS.
 
-    function formOpener(){
+    function formOpener() {
         formHandler(true)
     }
-    function formCloser(event){
+    function formCloser(event) {
         event.preventDefault();
+        setIsValid(true);
         formHandler(false);
     }
     let form = (
-        <button onClick={formOpener} className= "goal-opener">Add Course Goal</button>
+        <button onClick={formOpener} className="goal-opener">Add Course Goal</button>
     );
 
     const [userInput, setUserInput] = useState({
         goal: '',
     });
 
-    function goalHandler(event){
+    function goalHandler(event) {
         setUserInput({
             goal: event.target.value,
-            id: Math.random(),
         });
     }
-    function submissionHandler(event){
+    function submissionHandler(event) {
         event.preventDefault();
+        setUserInput((prevState) => (
+            [{
+                ...prevState,
+                id: Math.random(),
+            }]
+        ))
+        if (userInput.goal.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
         props.getGoalData(userInput);
         setUserInput({
             goal: '',
             id: 0,
         })
+        setIsValid(true);
     }
     const userGoal = userInput.goal;
 
-    if(formChecker){
-        form = (<form className= "goal-form" onSubmit={submissionHandler}>
-        <div className="goal-form__container">
-            <h2 className="goal-form__header">Course Goal</h2>
-            <div className="goal-form__submission">
-                <input type={"text"} className="goal-input" onChange = {goalHandler} value={userGoal}></input>
-                <div className="goal-form__buttons">
-                    <button type="submit" className="goal-submission">Add Goal</button>
-                    <button className="goal-back" onClick={formCloser}>Cancel</button>
+    if (formChecker) {
+        form = (<form className="goal-form" onSubmit={submissionHandler}>
+            <div className="goal-form__container">
+                <label className="goal-form__header" style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+                <div className="goal-form__submission">
+                    <input type={"text"} className="goal-input" onChange={goalHandler} value={userGoal} style={{ background: !isValid ? 'salmon' : 'transparent', borderColor: !isValid ? 'red' : '#ccc' }}></input>
+                    <div className="goal-form__buttons">
+                        <button type="submit" className="goal-submission">Add Goal</button>
+                        <button className="goal-back" onClick={formCloser}>Cancel</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </form>)
+        </form >)
     }
-    return(form)    
+    return (form)
 }
 
 export default CourseForm;
